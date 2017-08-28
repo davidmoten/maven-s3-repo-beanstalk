5d94e46f55350b8718b9f60efa06d72a7fdeff05
Dave Moten <davidmoten@gmail.com>
2017-08-29 09:25:12 +1000

    dev

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ if [ $# -eq 0 ]
     echo "Usage: ./deploy.sh <mode>"
     exit 1
 fi
-mvn clean package aws:deployFileS3@file aws:deployCf@cf -Dmode=$MODE
+mvn clean package aws:deployFileS3@file aws:deployCf@cf -Dmode=$MODE $1 $2 $3 $4 $5 $6 $7 $8
